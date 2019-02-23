2cbacf5dac4d4636ae2dbe7c09b1c3b6aee11afa
Lorenzo <lnorrid@gmail.com>
2019-02-23 12:04:48 -0700

    Created class to store recipe info when passing from db to page

---
+++ recipeManager/recipemanager/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace recipemanager
+{
+    public class Recipe
+    {
+        public int recipeID;
+        public int userID;
+        //have to pull arrays as string because they are stored as strings
+        //will need to be split using JS into multiple strings and placed in an array
+        public string ingredients;
+        public string utensils;
+        public string directions;
+    }
+}
