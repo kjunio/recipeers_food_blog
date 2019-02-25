d267f39df58626db2d9f7649fa5aeb34751a90c6
yimingguo1994 <45400906+yimingguo1994@users.noreply.github.com>
2019-02-25 11:19:29 -0700

    Update CreateRecipe.js

--- recipeManager/recipemanager/js/CreateRecipe.js
+++ recipeManager/recipemanager/js/CreateRecipe.js
@@ -1,23 +1,78 @@
 
 "use strict";
-$("#addIngredient").on("click", function(event)//adds new item to ingredient list
-{
-    var ingredientName = $("#ingredientNameInput").val();//grab ingredient name from input
-    var amountUsed = $("#amountUsedInput").val();//grab amount used from input
-	$("#ingredientList").append($('<li/>',{'class':'ingredientListItem', 'text': amountUsed + " " + ingredientName}));//appends item to existing list
-});
+var intergredientButton = document.getElementById('addIngredient');
+intergredientButton.onclick = addIntegredient;
+var addUtensilbtn = document.getElementById('addUtensil');
+addUtensilbtn.onclick = addUtensil;
+var addDirectionbtn = document.getElementById('addStep');
+addDirectionbtn.onclick = addDirection;
 
-$("#addUtensil").on("click", function(event)//adds new item to utensil list
-{
-    var utensilDescription = $("#utensilDescriptionInput").val();//grab utensil description from input
-	$("#untesilList").append($('<li/>',{'class':'utensilListItem', 'text': utensilDescription}));//appends item to existing list
-});
 
-$("#addStep").on("click", function(event)//adds new item to step list
+
+ function addIntegredient()//adds new item to ingredient list
 {
-    var stepDescription = $("#stepDescriptionInput").val();//grab step description from input
-	$("#stepList").append($('<li/>',{'class':'stepListItem', 'text': stepDescription}));//appends item to existing list
-});
+    var ul = document.getElementById('ingredientList')
+     var li = document.createElement('li');
+    
+    //grab ingredient name from input
+    var amountUsed = document.getElementById('amountUsedInput'); //grab amount used from input
+    var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.value = 1;
+        checkbox.name = "todo[]";
+
+    li.appendChild(checkbox);
+    var ingredientName = document.getElementById('ingredientNameInput');
+    li.appendChild(document.createTextNode(ingredientName.value +" "+ amountUsed.value));
+    ul.appendChild(li); 
+	//appends item to existing list
+    
+    
+    document.getElementById('ingredientNameInput').value = "";
+    document.getElementById('amountUsedInput').value = "";
+};
+
+            
+
+function addUtensil()//adds new item to ingredient list
+{
+    var ul = document.getElementById('untesilList');
+    var li = document.createElement('li');
+    
+    //grab ingredient name from input
+    var utensilDesc = document.getElementById('utensilDescriptionInput'); //grab amount used from input
+    var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.value = 1;
+        checkbox.name = "todo[]";
+
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(utensilDesc.value));
+    ul.appendChild(li); 
+    //appends item to existing list
+    document.getElementById('utensilDescriptionInput').value = "";
+ 
+};
+
+function addDirection()//adds new item to ingredient list
+{
+    var ol = document.getElementById('stepList');
+    var li = document.createElement('li');
+    
+    //grab ingredient name from input
+    var steps = document.getElementById('stepDescriptionInput'); //grab amount used from input
+    var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.value = 1;
+        checkbox.name = "todo[]";
+
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(steps.value));
+    ol.appendChild(li); 
+    //appends item to existing list
+    document.getElementById('stepDescriptionInput').value = "";
+ 
+};
 
 function PassCreateRecipeValues() {
     var recipeName = $("#recipeNameInput").val();//stores name
