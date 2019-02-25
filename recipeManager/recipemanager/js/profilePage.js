c68c8bf2c1a9bfcfa46fb18e05266d3f06f6de38
jkelly3 <jkelly3@asu.edu>
2019-02-25 13:24:21 -0700

    Worked on Profile Page js and Services
    -Added profilePage.js
    -Edited profilePage.html
    -Added GetProfileRecipes & GetProfile
    to the RecipeServices.asmx.cs

---
+++ recipeManager/recipemanager/js/profilePage.js
@@ -0,0 +1,43 @@
+window.onload = function isUserLoggedIn() {
+    var webMethod = "../RecipeServices.asmx/GetProfile";
+    $.ajax({
+        type: "GET",
+        url: webMethod,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {            
+            var profile = msg.d;
+        }
+    });
+    if (!profile.session) {
+        alert("Please login to view Profile.");
+        //window.open("Signin.html","_self")
+        return;
+    }
+    let userId = profile.userId;
+    let email = profile.email;
+    updateUserInfo(userId, email);
+    getUserRecipes(userId);
+}
+function updateUserInfo(userId, email) {
+    document.getElementById("userInfo").innerHTML = "<b>" + userId + "<br>" + email + "</b></h3>";
+}
+function getUserRecipes(userId) {
+    var webMethod = "../RecipeServices.asmx/GetProfileRecipes";
+    var parameters = "{\"username\":\"" + encodeURI(username) + "\"}";
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        data: parameters,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {            
+            var recipes = msg.d;
+        }
+    });
+    if (recipes.recipeId != Null) {
+        let recipeId = recipes.recipeId;
+        let recipeName = recipes.recipeName;
+        document.getElementById("userRecipes").innerHTML = "<li>" + recipeName + "</li>";
+    }
+}
